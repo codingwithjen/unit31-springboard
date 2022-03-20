8bd8bfbe5cb2f91e58ad966d302760ad28f5489d
jen f <77820429+jenfausto@users.noreply.github.com>
2022-03-19 20:17:42 -0600

    step1.js complete, step2.js being worked out

---
+++ node-files/step2.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const process = require('process')
+const axios = require('axios').default;
+
+// Write a function, cat. It should take one argument, path, and it should read the file with that
+// path, and print the contents of the file. Then, write some code that calls that function,
+// allowing you to specify the path argument via the command line
+function cat(path) {
+    fs.readFile(path, 'utf8', function(err, data) {
+        if (err) {
+            // handle possible error
+            console.error(err);
+            // kill the process and tell the shell it errored
+            process.exit(1);
+        } else {
+            console.log(data);
+        }
+    });
+}
+
+function webCat
+// cat(process.argv[1]);
+cat(process.argv[2]);
