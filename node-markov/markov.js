ccdc9842438d44cd8582baabf9548d9daf9bf424
jen f <77820429+jenfausto@users.noreply.github.com>
2022-03-21 22:42:27 -0600

    step one completed

--- node-markov/markov.js
+++ node-markov/markov.js
@@ -4,6 +4,8 @@
 class MarkovMachine {
 
   /** build markov machine; read in text.*/
+  // The constructor function contains code; it splits it on spaces and linebreak characters to make
+  // a list of words...
 
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
@@ -18,12 +20,46 @@ class MarkovMachine {
 
   makeChains() {
     // TODO
+    let chains = {};
+    for (let i = 0; i < this.words.length; i++) {
+      let word = this.words[i];
+      // show that we if picked null, we've reached the end of the chain, so stop
+      let nextWord = this.words[i + 1] || null;
+
+      if (chains[word]) {
+        if (chains[word].indexOf(nextWord) === -1) chains[word].push(nextWord);
+      } else {
+        chains[word] = [nextWord];
+      }
+    }
+    this.chains = chains;
   }
+
 
 
   /** return random text from chains */
 
   makeText(numWords = 100) {
     // TODO
+    // pick a random key to begin
+    let keys = Object.keys(this.chains);
+    let key = keys[Math.floor(Math.random() * Math.floor(keys.length))];
+    let out = [];
+
+    // produce Markov chain until reaching termination word
+    while (out.length < numWords && key !== null) {
+      out.push(key);
+      key = MarkovMachine.choice(this.chains.get(key));
+    }
+    return out.join(" ");
   }
 }
+
+// Export contents of files with module.exports (which is usually an object)
+module.exports = {
+  MarkovMachine,
+};
+
+let mm = new MarkovMachine("the cat in the cat");
+mm.makeText();
+// mm.makeText(numwords=50);
